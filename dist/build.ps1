28b131b868b4e4cf8ad4a39303c62c6e83d58b04
The Sharp Ninja <ninja@thesharp.ninja>
2021-11-01 20:35:10 +0000

    Passing variables to PS1.

--- dist/build.ps1
+++ dist/build.ps1
@@ -1,3 +1,11 @@
 #!pwsh
 
-echo 'Powershell Hello'
+$rootPath = $env:BUILD_ROOT_PATH
+$configuration = $env:BUILD_CONFIGURATION
+$platform = $env:BUILD_PLATFORM
+[switch]$restore = Boolean.Parse($env:BUILD_RESTORE)
+
+Write-Host "`$rootPath: $rootPath"
+Write-Host "`$configuration: $configuration"
+Write-Host "`$platform: $platform"
+Write-Host "`$restore: $restore"
--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export BUILD_ROOT_PATH=$1
+export BUILD_CONFIGURATION=$2
+export BUILD_PLATFORM=$3
+export BUILD_RESTORE=$4
+
 chmod +x /build.ps1
 
 pwsh /build.ps1
