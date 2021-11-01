2a2930c6ea6d0f9507735c4400ca7fd0797ff959
The Sharp Ninja <ninja@thesharp.ninja>
2021-11-01 22:11:31 +0000

    Call script directly.

--- dist/build.ps1
+++ dist/build.ps1
@@ -14,9 +14,13 @@ try {
     if (Test-Path $rootPath) {
         #download Dotnet
         # Run a separate PowerShell process because the script calls exit, so it will end the current PowerShell session.
-        $parameters = '-Channel 6.0 -Quality preview -InstallDir ./dotnet -Architecture x64 -Verbose';
-        & pwsh -NoProfile -ExecutionPolicy unrestricted `
-            -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) $parameters" 
+        # $parameters = '-Channel 6.0 -Quality preview -InstallDir ./dotnet -Architecture x64 -Verbose';
+        # & pwsh -NoProfile -ExecutionPolicy unrestricted `
+        #     -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) $parameters" 
+
+        Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile ./dotnet-install.ps1 -Verbose
+
+        . ./dotnet-install.ps1 -Channel 6.0 -Quality preview -InstallDir ./dotnet -Architecture x64 -Verbose
 
         if($LASTEXITCODE -ne 0){
             throw "Powershell returned $LASTEXITCODE installing dotnet.";
